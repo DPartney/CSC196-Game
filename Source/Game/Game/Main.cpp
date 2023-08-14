746891d972bb15d73bfda93f8d173f0ed6ab7aa0
DPartney <dpartney@student.neumont.edu>
2023-08-14 13:42:23 -0600

    Json Working

--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -43,11 +43,34 @@ public:
 
 int main(int argc, char* argv[])
 {
-	INFO_LOG("Initiliaze Engine");
+	INFO_LOG("Initailizing Engine...");
 
 	kiko::MemoryTracker::Initialize();
 	kiko::seedRandom((unsigned int)time(nullptr));
 	kiko::setFilePath("assets");
+
+	/* Json Functions Working!
+	rapidjson::Document document;
+	kiko::Json::Load("json.txt", document);
+	int i1;
+	kiko::Json::Read(document, "integer1", i1);
+	std::cout << i1 << std::endl;
+	int i2;
+	kiko::Json::Read(document, "integer2", i2);
+	std::cout << i2 << std::endl;
+	std::string str;
+	kiko::Json::Read(document, "string", str);
+	std::cout << str << std::endl;
+	bool b;
+	kiko::Json::Read(document, "boolean", b);
+	std::cout << b << std::endl;
+	float f;
+	kiko::Json::Read(document, "float", f);
+	std::cout << f << std::endl;
+	kiko::vec2 v2;
+	kiko::Json::Read(document, "vector2", v2);
+	std::cout << v2 << std::endl;
+	*/
 
 	// initialize engine
 	kiko::g_renderer.Initialize();
